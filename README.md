7c9ccee2f5b3fe8e21ba5c4a9ccbe8457b5e6d48
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2021-11-01 00:16:23 +0600

    refactor(readme, assets, codes): ready for github

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Smoke Alert
- 
+# To The Rescue
+
+This is a monitoring system for possible gas leakage and fire outbreak.
+
+<br/>
+<img width="700" src="assets/poster.jpg"/>
+<br/>
+
+## Competition:
+
+Participated in Subornojoyonti Hackathon-2021.
--- codes/jrc/jrc.ino
+++ codes/jrc/jrc.ino
@@ -5,7 +5,7 @@
 
 #define analogLow 0
 #define analogHigh 1024
-#define gasReadLimit 1000
+#define gasReadLimit 900
 
 #define flameReadPin1 0
 #define flameReadPin2 4
@@ -21,7 +21,7 @@
 #define SWITCHOFF 2
 
 //lib instances
-LiquidCrystal lcd(18,19,23,13,14,16);
+LiquidCrystal lcd(18, 19, 23, 13, 14, 16);
 dht DH;
 SoftwareSerial mySerial(15, 17); //SIM800L Tx & Rx is connected to Arduino #3 & #2
 
@@ -38,115 +38,149 @@ String gasOutput = "Safe";
 
 int command = -1;
 
-void handleCommand(){
-  switch(command){
-    case STATUS:
-      sendMsg(line11+line12+line13+line14+line15);
-      break;
-    case SWITCHON:
-      motorOn();
-      break;
-    case SWITCHOFF:
-      motorOff();
-      break;
+void handleCommand()
+{
+  switch (command)
+  {
+  case STATUS:
+    sendMsg(line11 + line12 + line13 + line14 + line15);
+    break;
+  case SWITCHON:
+    motorOn();
+    break;
+  case SWITCHOFF:
+    motorOff();
+    break;
   }
+  command = -1;
 }
 
-void sendMsg(String msg){
+void sendMsg(String msg)
+{
   mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
   updateSerial();
   mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
   updateSerial();
-  mySerial.println("AT+CMGS=\"+8801633927378\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+  mySerial.println("AT+CMGS=\"+8801633927378\""); //change ZZ with country code and xxxxxxxxxxx with phone number to sms
   updateSerial();
   mySerial.print(msg); //text content
   updateSerial();
   mySerial.write(26);
 }
 
-void sendCall(){
+void sendCall()
+{
   mySerial.println("AT"); //Once the handshake test is successful, i t will back to OK
   updateSerial();
-  
+
   mySerial.println("ATD+ +8801633927378;"); //  change ZZ with country code and xxxxxxxxxxx with phone number to dial
   updateSerial();
-  delay(20000); // wait for 20 seconds...
+  delay(20000);            // wait for 20 seconds...
   mySerial.println("ATH"); //hang up
   updateSerial();
 }
 
 void updateSerial()
 {
+  String gotMsg;
   delay(500);
-  while (Serial.available()) 
+  while (Serial.available())
   {
-    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+    mySerial.write(Serial.read()); //Forward what Serial received to Software Serial Port
   }
-  while(mySerial.available()) 
+  while (mySerial.available())
   {
-    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+    gotMsg += mySerial.read();
+  }
+  if (gotMsg.length() > 0)
+  {
+    Serial.println(gotMsg); //Forward what Software Serial received to Serial Port
+    updCommand(gotMsg);
   }
 }
 
-void updateDisplay(){
+void updCommand(String gotMsg)
+{
+  if (gotMsg.substring(gotMsg.length() - 5) == "status")
+  {
+    command = STATUS;
+  }
+  else if (gotMsg.substring(gotMsg.length() - 2) == "on")
+  {
+    command = SWITCHON;
+  }
+  else if (gotMsg.substring(gotMsg.length() - 3) == "off")
+  {
+    command = SWITCHOFF;
+  }
+  handleCommand();
+}
+
+void updateDisplay()
+{
   lcd.clear();
-  if(curDisp == 0){
-    lcd.setCursor(0,0);
+  if (curDisp == 0)
+  {
+    lcd.setCursor(0, 0);
     lcd.print(line11);
   }
-  else if(curDisp == 1){
-    lcd.setCursor(0,0);
+  else if (curDisp == 1)
+  {
+    lcd.setCursor(0, 0);
     lcd.print(line12);
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print(line13);
   }
-  else{
-    lcd.setCursor(0,0);
+  else
+  {
+    lcd.setCursor(0, 0);
     lcd.print(line14);
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print(line15);
   }
 }
 
-void motorOn(){
+void motorOn()
+{
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor2pin1, HIGH);
   analogWrite(motor1speed, 255);
   analogWrite(motor2speed, 255);
 }
 
-void motorOff(){
+void motorOff()
+{
   digitalWrite(motor1pin1, LOW);
   digitalWrite(motor2pin1, LOW);
   analogWrite(motor1speed, 0);
   analogWrite(motor2speed, 0);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   mySerial.begin(9600);
-  lcd.begin(16,2);
-  
+  lcd.begin(16, 2);
+
   lcd.clear();
-  
+
   Serial.println("Serial Monitor Started.");
 
-  pinMode(flameReadPin1,INPUT);
-  pinMode(flameReadPin2,INPUT);
+  pinMode(flameReadPin1, INPUT);
+  pinMode(flameReadPin2, INPUT);
   pinMode(gasReadPin, INPUT);
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor2pin1, OUTPUT);
   pinMode(motor1speed, OUTPUT);
   pinMode(motor2speed, OUTPUT);
 
-  sendMsg("Hola");
+  //sendMsg("Hola");
   delay(1000);
 }
 
-
-
-void loop() {
-  curDisp = (curDisp + 1)%3;
+void loop()
+{
+  curDisp = (curDisp + 1) % 3;
 
   //read sensor
   int flameRead1 = digitalRead(flameReadPin1);
@@ -155,7 +189,7 @@ void loop() {
   DH.read11(dhtReadPin);
   //end of read sensor
 
-  /*Serial.print("flameRead1: ");
+  Serial.print("flameRead1: ");
   Serial.println(flameRead1);
   Serial.print("flameRead2: ");
   Serial.println(flameRead2);
@@ -164,42 +198,50 @@ void loop() {
   Serial.print("humidity = ");
   Serial.println(DH.humidity);
   Serial.print("temperature: ");
-  Serial.print(DH.temperature); 
-  Serial.println("C  ");*/
+  Serial.print(DH.temperature);
+  Serial.println("C  ");
 
   //flame out
-  if(flameRead1 == LOW or flameRead2 == LOW){
+  if (flameRead1 == LOW or flameRead2 == LOW)
+  {
+    Serial.println("fire");
     line11 = "Status: Fire!";
     flameOutput = "Fire";
     motorOn();
-    sendMsg(line11+line12+line13+line14+line15);
+    sendMsg(line11 + line12 + line13 + line14 + line15);
   }
-  else{
-    line11 = "Status: Normal";
+  else
+  {
     flameOutput = "Safe";
     motorOff();
   }
   //end of flame out
 
   //gas leakage
-  if(gasAnalogRead > gasReadLimit){
+  if (gasAnalogRead > gasReadLimit)
+  {
     line11 = "Status: Gas Leak!";
     gasOutput = "Danger";
     motorOn();
-    sendMsg(line11+line12+line13+line14+line15);
+    sendMsg(line11 + line12 + line13 + line14 + line15);
   }
-  else{
-    line11 = "Status: Normal";
+  else
+  {
     gasOutput = "Safe";
     motorOff();
   }
   //end of gas leakage
-  
+
+  if (gasAnalogRead <= gasReadLimit and flameRead1 == HIGH and flameRead2 == HIGH)
+  {
+    Serial.println("n");
+    line11 = "Status: Normal";
+  }
+
   //display update
-  line13 = flameOutput + String("    ")+String(gasOutput);
-  line15 =  String(DH.temperature) + String("C ") + String(DH.humidity) + "%";
+  line13 = flameOutput + String("    ") + String(gasOutput);
+  line15 = String(DH.temperature) + String("C ") + String(DH.humidity) + "%";
   updateDisplay();
-  
+
   delay(1500);
-  
 }
